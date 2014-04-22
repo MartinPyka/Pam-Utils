b2d78051b0e5a154d099ffd89401174293cb76c7
Sebastian Klatt <sebastian@markow.org>
2014-04-22 14:03:23 +0200

    Added requirements for pypi submission. There is still meta data missing.

---
+++ setup.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+source_path = os.path.abspath("src")
+sys.path.append(source_path)
+
+import pam2nest
+
+packages = ["pam2nest"]
+requires = []
+
+with open("README") as f:
+    readme = f.read()
+with open("LICENSE") as f:
+    license = f.read()
+
+setup(
+    name="pam2nest",
+    version=pam2nest.__version__,
+    description="",  # TODO(SK): description missing
+    long_description=readme,
+    author="Sebastian Klatt"
+    author_email="sebastian.klatt@rub.de",
+    url="",  # TODO(SK): url missing
+    packages=packages,
+    package_data={"": ["LICENSE"]},
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=requires,
+    license="GPL v2",
+    zip_safe=False,
+    classifiers=(
+        # TODO(SK): classifiers missing
+        # TODO(SK): full list is available at:
+        # TODO(SK): https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+    ),
+)
