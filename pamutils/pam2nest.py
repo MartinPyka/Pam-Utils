abb328fea944bee79019f0e5729592d349c576cb
MartinPyka <martin.pyka@gmx.de>
2015-07-14 22:20:15 +0200

    port to nest2.6 for now working

--- pamutils/pam2nest.py
+++ pamutils/pam2nest.py
@@ -113,19 +113,13 @@ def Connect(g1, g2, connections, delays,
             if connections[i][j] >= 0:
                 weight = np.random.randn(1)[0] * w_sd + w_mean
                 if w_mean > 0:
-                    weight = max(weight, 0)
+                    weight = max(weight, 0.1)
                 if w_mean < 0:
-                    weight = min(weight, 0)
+                    weight = min(weight, 0.1)
                     
                 # delay = np.random.randn(1)[0] * d_sd + d_mean
                 delay = max(delays[i][j] * delay_mm, 0.1)
-                print((g1[i]))
-                print((g2[connections[i][j]]))
-                nest.Connect([g1[i]], [g2[connections[i][j]]],
-                             syn_spec = {'model': syn_model,
-                                         'weight': weight,
-                                         'delay': int(delay)}
-                             )
+                nest.Connect([g1[i]], [g2[connections[i][j]]], syn_spec = {'model': syn_model, 'weight': weight, 'delay': delay})
 #                nest.DataConnect([g1[i]], 
 #                                 params=[{'target': [float(g2[connections[i][j]])],
 #                                         'weight': [np.random.randn(1)[0] * w_sd + w_mean], 
